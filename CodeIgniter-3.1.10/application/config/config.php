d46d933165de2d345c9c270b7bb73ea691a7a3c9
liuxiao11 <945480207@qq.com>
2020-01-13 19:50:14 +0800

    1.13

--- CodeIgniter-3.1.10/application/config/config.php
+++ CodeIgniter-3.1.10/application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://local.ci.com/';
+$config['base_url'] = 'http://local.air.com/';
 
 /*
 |--------------------------------------------------------------------------
